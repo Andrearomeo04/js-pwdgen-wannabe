23bc8a55bdac474f5a1985605fc54139db0f1126
andrearomeo04 <pisco004@gmail.com>
2024-06-24 17:20:41 +0200

    [UPD] removed spaces

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ let nome = prompt('inserisci il tuo nome');
 let surname = prompt('inserisci il tuo cognome');
 let fav_color = prompt('inserisci il tuo colore preferito');
 
-let full_string = `${nome} ${surname} ${fav_color} 23`
+let full_string = `${nome}${surname}${fav_color}23`
 document.getElementById('template_literal').innerHTML = full_string
